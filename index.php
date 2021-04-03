96ee8a099314afaa85a93837c90424f994aa884b
Patrick Maher <patrickemaher@yahoo.com>
2021-04-03 11:48:40 -0700

    ACreating a get method to retrieve addresses.

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-    echo 'test'
+
+    session_start();
+    header('Content-Type: application/json');
+    
+    switch($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            initializeAddressesIfNotSet();
+            echo json_encode($_SESSION['addresses'], JSON_PRETTY_PRINT);
+            break;
+        default:
+            $error = array('error'=>'Operation not supported.');
+            echo json_encode($error, JSON_PRETTY_PRINT);
+    }
+
+    function initializeAddressesIfNotSet() {
+        if(!isset($_SESSION['addresses'])) {
+            $jsonurl='./addresses.json'; 
+            $json = file_get_contents($jsonurl,true); 
+            $someArray = json_decode($json, JSON_PRETTY_PRINT); 
+            $_SESSION['addresses'] = $someArray;
+        }
+    }
 ?>
