675d9976066543fcdaccfb748f17939df7d4a770
Patrick Maher <patrickemaher@yahoo.com>
2021-04-13 21:49:43 -0700

    Implementing the put method.

--- index.php
+++ index.php
@@ -35,11 +35,7 @@
                 $error = array('error'=>'Address id not found.');
                 echo json_encode($error, JSON_PRETTY_PRINT);
             } else {
-
-                
                 $address = getAddressById($bodyArray['addressId']);
-                echo $bodyArray['addressId'];
-                break;
                 if(is_null($address)) {
                     $error = array('error'=>'Address with id ' . strval($bodyArray['addressId']) . ' not found.');
                     echo json_encode($error, JSON_PRETTY_PRINT);
@@ -50,8 +46,22 @@
             }
             break;
         case 'PUT':
-            $error = array('error'=>'Not yet implemented.');
-            echo json_encode($error, JSON_PRETTY_PRINT);
+            $_POST = json_decode(file_get_contents("php://input"), true);
+
+            if(empty($_POST['address'])) {
+                $error = array('error'=>'Operation requires an address.');
+                echo json_encode($error, JSON_PRETTY_PRINT);
+            } else {
+                initializeAddressesIfNotSet();
+                $address = $_POST['address'];
+                $success = updateAddress($address);
+                if($success) {
+                    echo json_encode($address, JSON_PRETTY_PRINT);
+                } else {
+                    $error = array('error'=>'Address id not found.');
+                    echo json_encode($error, JSON_PRETTY_PRINT);
+                }
+            }
             break;
         case 'GET':
             initializeAddressesIfNotSet();
@@ -71,8 +81,27 @@
         $_SESSION['addresses'] = array_values($newArray);
     }
 
+    function updateAddress($address) {
+        $id = $address['id'];
+        $success = false;
+        foreach ($_SESSION['addresses'] as &$savedAddress) {
+            if($savedAddress['id'] == $id) {
+                $success = true;
+                $savedAddress['first_name'] = $address['first_name'];
+                $savedAddress['last_name'] = $address['last_name'];
+                $savedAddress['address_1'] = $address['address_1'];
+                $savedAddress['address_2'] = $address['address_2'];
+                $savedAddress['city'] = $address['city'];
+                $savedAddress['state'] = $address['state'];
+                $savedAddress['zip'] = $address['zip'];
+                $savedAddress['phone'] = $address['phone'];
+                $savedAddress['email'] = $address['email'];
+            }
+        }
+        return $success;
+    }
+
     function getAddressById($id) {
-        echo $id;
         foreach ($_SESSION['addresses'] as &$address) {
             if($address['id'] == $id) {
                 return $address;
